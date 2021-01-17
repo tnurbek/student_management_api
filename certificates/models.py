e755020a59854e2e3df9118328aad488b613fa93
user <user@example.com>
2021-01-17 19:05:13 +0600

    added certificates and timestamps

---
+++ certificates/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from utils.models import Timestamp
+
+class Certificate(Timestamp, models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    pass
