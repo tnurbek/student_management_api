c772fdf80f242c90152298d9ddb4e81a1462a03c
user <user@example.com>
2021-01-17 19:10:13 +0600

    changed cert models.py

--- certificates/models.py
+++ certificates/models.py
@@ -4,8 +4,6 @@ from utils.models import Timestamp
 class Certificate(Timestamp, models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name.lower()
